fe04c16f5561a790469208fb6999639079c67968
Jefe21054 <ivaniglesias500@protonmail.ch>
2023-05-04 23:50:54 -0500

    Mostrar Productos

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log(`HOLA`)
+const productos = [
+    {
+        nombre: 'Hamburguesa Simple',
+        costo: 20,
+        codigo: 'H100'
+    }, 
+    {
+        nombre: 'Hamburguesa Doble',
+        costo: 30,
+        codigo: 'H200'
+    }, 
+    {
+        nombre: 'Papas fritas',
+        costo: 8,
+        codigo: 'P100'
+    },
+    {
+        nombre: 'Refresco Personal',
+        costo: 6,
+        codigo: 'R100'
+    }
+]
+
+const mostrarMenu = () => {
+    console.log(`CODIGO - NOMBRE PRODUCTO - COSTO`)
+    for (let producto of productos) {
+        console.log(`${producto.codigo} - ${producto.nombre} - $${producto.costo}`)
+    }
+}
