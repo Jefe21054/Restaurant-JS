8f2fefd30f1660d84f7173fb77510073c8bfca7b
Jefe21054 <ivaniglesias500@protonmail.ch>
2023-05-05 09:51:41 -0500

    Código Estandarizado con ESLint

--- index.js
+++ index.js
@@ -4,23 +4,23 @@
  */
 
 const usuario = {
-    nombre: 'Ivan',
-    edad: 24,
-    deuda: 0
+  nombre: 'Ivan',
+  edad: 24,
+  deuda: 0
 }
 
 let pedido = []
 let costoPedido = 0
 
 /**
- * Lista de todos los productos del menu en un 
+ * Lista de todos los productos del menu en un
  * formato amigable
 */
 const mostrarMenu = () => {
-    console.log(`CODIGO - NOMBRE PRODUCTO - COSTO`)
-    for (let producto of productos) {
-        console.log(`${producto.codigo} - ${producto.nombre} - $${producto.costo}`)
-    }
+  console.log('CODIGO - NOMBRE PRODUCTO - COSTO')
+  for (const producto of productos) {
+    console.log(`${producto.codigo} - ${producto.nombre} - $${producto.costo}`)
+  }
 }
 
 /**
@@ -28,60 +28,60 @@ const mostrarMenu = () => {
  * del menu mostrado con anterioridad
  */
 const pedirProducto = codigo => {
-    if (!codigo || typeof codigo === 'number' || typeof codigo === 'boolean') return 'Ingrese un codigo valido'
+  if (!codigo || typeof codigo === 'number' || typeof codigo === 'boolean') return 'Ingrese un codigo valido'
 
-    const productoEncontrado = productos.find(producto => producto.codigo === codigo)
-    if (!productoEncontrado) return 'El producto no existe'
+  const productoEncontrado = productos.find(producto => producto.codigo === codigo)
+  if (!productoEncontrado) return 'El producto no existe'
 
-    pedido.push(productoEncontrado)
-    console.log('El producto ha sido agregado a su pedido. Su pedido es:')
-    return verPedido()
+  pedido.push(productoEncontrado)
+  console.log('El producto ha sido agregado a su pedido. Su pedido es:')
+  return verPedido()
 }
 
 // Funcion que permite ver un pedido
 const verPedido = () => pedido
 
 /**
- * Funcion que permite calcular el costo de un pedido 
+ * Funcion que permite calcular el costo de un pedido
  * completo realizado por el usuario
  */
 const calcularCosto = () => {
-    let costo = 0
-    for (unidad of pedido) {
-        costo += unidad.costo
-    }
-    costoPedido = costo
-    return costoPedido
+  let costo = 0
+  for (unidad of pedido) {
+    costo += unidad.costo
+  }
+  costoPedido = costo
+  return costoPedido
 }
 
 // Funcion que permite finalizar un pedido
 const finalizarPedido = () => {
-    calcularCosto()
-    usuario.deuda = costoPedido
+  calcularCosto()
+  usuario.deuda = costoPedido
 
-    pedido = []
-    costoPedido = 0
+  pedido = []
+  costoPedido = 0
 
-    return `${usuario.nombre}, debes pagar $${usuario.deuda}`
+  return `${usuario.nombre}, debes pagar $${usuario.deuda}`
 }
 
 /**
- * Funcion que permite pagar todo un pedido y 
+ * Funcion que permite pagar todo un pedido y
  * entregar cambio si fuera necesario
  */
 const pagarPedido = montoEntregado => {
-    if (typeof montoEntregado === 'number'){
-        if (montoEntregado < usuario.deuda){
-            return `No te alcanza para pagar tu pedido`
-        }else if (montoEntregado === usuario.deuda) {
-            usuario.deuda = 0
-            return `Tu pedido ha sido pagado`
-        } else {
-            console.log(`Tu pedido ha sido pagado y tu cambio es de $${montoEntregado - usuario.deuda}`)
-            usuario.deuda = 0
-            return `Deuda pagada`
-        }
+  if (typeof montoEntregado === 'number') {
+    if (montoEntregado < usuario.deuda) {
+      return 'No te alcanza para pagar tu pedido'
+    } else if (montoEntregado === usuario.deuda) {
+      usuario.deuda = 0
+      return 'Tu pedido ha sido pagado'
     } else {
-        return `Dato ingresado de forma erronea`
+      console.log(`Tu pedido ha sido pagado y tu cambio es de $${montoEntregado - usuario.deuda}`)
+      usuario.deuda = 0
+      return 'Deuda pagada'
     }
+  } else {
+    return 'Dato ingresado de forma erronea'
+  }
 }
